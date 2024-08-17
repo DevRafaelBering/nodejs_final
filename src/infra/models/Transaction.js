740b4e2a2a252f3af8ac6b9dbea995fd3f9db41c
DevRafaelBering <rafael_bering99@hotmail.com>
2024-08-16 22:22:30 -0300

    Atualizando o ponto

--- src/infra/models/Transaction.js
+++ src/infra/models/Transaction.js
@@ -1,12 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const TransactionSchema = new mongoose.Schema({
-  
-  title: String,
-  type: String,
-  category: String,
-  amount: Number,
-
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: ["deposit", "withdraw"],
+    default: "withdraw",
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
 });
 
-module.exports = mongoose.model('Transaction', TransactionSchema);
+module.exports = mongoose.model("Transaction", TransactionSchema);
